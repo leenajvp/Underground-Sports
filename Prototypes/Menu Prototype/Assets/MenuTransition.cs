dde32063ef6b006d923ee57cf46e39c5c6bbb1ff
ksaonsd <sjdsad>
2020-02-06 11:05:29 +0000

    Added menu prototype

---
+++ Prototypes/Menu Prototype/Assets/MenuTransition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuTransition : MonoBehaviour
+{
+    //public GameObject Logo;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine("Switch");
+    }
+
+    IEnumerator Switch()
+    {
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("LDScreen");
+    }
+}
