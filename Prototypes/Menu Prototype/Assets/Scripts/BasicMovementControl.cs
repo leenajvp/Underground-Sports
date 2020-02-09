f5d4fe34e2aa9dac2c727d8e2138a186ed879d00
Leena <lj3827h@gre.ac.uk>
2020-02-09 14:55:20 +0000

    Buttons, Logo, Movement Script and map

---
+++ Prototypes/Menu Prototype/Assets/Scripts/BasicMovementControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovementControl : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody2D rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        transform.position = new Vector2(0, 0);
+    }
+
+    // Update is called once per frame
+    //Basic movement
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float jumpVelocity = 10f;
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+    void CalculateMovement()
+    { 
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector2.right * horizontalInput * Speed * Time.deltaTime);
+    }
+    
+}
+
---
+++ Prototypes/Menu Prototype/Assets/Scripts/PacuJawiMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacuJawiMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float HorizontalMoveSpeed = 1f;
+    public float VerticalMoveSpeed = 0.1f;
+    private Rigidbody2D rb;
+    Vector2 direction = new Vector2(10f, 0f);
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        //transform.position += Vector3.right * HorizontalMoveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * VerticalMoveSpeed;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * VerticalMoveSpeed;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = direction * HorizontalMoveSpeed;
+    }
+    //When reach goal stop
+}
