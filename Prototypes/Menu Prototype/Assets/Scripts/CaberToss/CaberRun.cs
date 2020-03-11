08079ecce9e1f562dd01527b3c789fe610a3c365
Josh H-H <jh3771u@greenwich.ac.uk>
2020-03-11 12:20:48 +0000

    null.0

---
+++ Prototypes/Menu Prototype/Assets/Scripts/CaberToss/CaberRun.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaberRun : MonoBehaviour
+{
+
+    public float HorizontalMoveSpeed = 0.01f;
+    public bool canMove = true;
+    
+    void Update()
+    {
+        if (canMove == true)
+        {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position += Vector3.right * HorizontalMoveSpeed;
+                //character movement
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision){
+        if(collision.gameObject.tag == ("StopMoving")){
+            canMove = false;
+            Debug.Log("hti");
+        }
+    }
+    
+    /*private void OnTriggerEnter2D(Collider2D other)
+    {
+        Freezer();
+        Debug.Log("slowed");
+    }
+
+    void Freezer()
+    {
+        HorizontalMoveSpeed = 0.00000000f;
+        //sets character movement to 0
+    }*/
+}
---
+++ Prototypes/Menu Prototype/Assets/Scripts/CaberToss/GlobalData.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GlobalData : MonoBehaviour
+{
+    static public int Scoring;
+    
+    public Text scoreText;
+    
+    void Awake()
+    {
+        Scoring = 0;
+        SetDistance();
+    }
+    
+    public void SetDistance()
+    {
+        scoreText.text = Scoring.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
